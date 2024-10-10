21dc759e557de2999ae5c3491f740a61bb7038cb
Pedro Arnor Oliveira <p.oliviras@gmail.com>
2024-10-10 11:34:16 -0300

    Add Sistema de Hospedagem

---
+++ SistemaDeHospedagemDIO/Models/Reserva.cs
@@ -0,0 +1,55 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new ArgumentException("O número de hóspedes excede a capacidade da suíte.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes != null ? Hospedes.Count : 0; // Certifica-se de que a lista não é nula
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor -= valor * 0.10M; // Concede um desconto de 10%
+            }
+
+            return valor;
+        }
+    }
+}
